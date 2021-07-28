8d1f4bce576c36bbf9572f04088b8808ff40a5b8
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2021-07-28 13:36:06 +0530

    Update lpubg.js

--- plugins/lpubg.js
+++ plugins/lpubg.js
@@ -14,7 +14,7 @@ if (Config.WORKTYPE == 'private') {
 
         if (match[1] === '') return await message.sendMessage(need);
 
-        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/pubglogo?apikey=HYLQkGwhDYquQut8eIOvxyzcXHR&text1=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/pubglogo?apikey=HYLQkGwhDYquQut8eIOvxyzcXHR&text1=TEXT1%20BANG=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Amalser*' })
 
@@ -27,7 +27,7 @@ else if (Config.WORKTYPE == 'public') {
 
         if (match[1] === '') return await message.sendMessage(need);
 
-        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/pubglogo?apikey=HYLQkGwhDYquQut8eIOvxyzcXHR&text1=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/pubglogo?apikey=HYLQkGwhDYquQut8eIOvxyzcXHR&text1=TEXT1%20BANG=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by RFX JOKER*' })
 
